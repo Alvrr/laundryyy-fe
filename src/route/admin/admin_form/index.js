e06b373d0df7eecad9487066caa564a7018b10c3
farid <dekfarid2015@gmail.com>
2025-02-12 16:44:22 +0700

    test url

--- src/route/admin/admin_form/index.js
+++ src/route/admin/admin_form/index.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Swal from "sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
 import Sidebar from '../../../components/Sidebar';
+import { getBaseUrl } from "../../config";
 
 const Admin = () => {        
   const navigate = useNavigate();        
@@ -27,7 +28,7 @@ const Admin = () => {
   
   const fetchLayanan = async () => {      
     try {      
-      const response = await fetch('http://localhost:5000/layanan'); 
+      const response = await fetch(getBaseUrl(`/layanan`)); 
       const data = await response.json();      
       if (response.ok) {      
         setLayananList(data.data); 
